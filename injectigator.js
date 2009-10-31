8e51c983aa4db406f8a808ea77a5d091a9c294f4
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-10-30 23:27:32 -0700

    added comments and almost got the code going.. going to add unit tests

--- injectigator.js
+++ injectigator.js
@@ -1,29 +1,82 @@
+/**
+ * @fileoverview The Injectigator JS API.
+ * @author ibolmo@gmail.com (Olmo Maldonado)
+ */
+
+
 (function() {
 
-var II = this.Injectigator = {};
+/**
+ * @type {Object} The Injectigator structure.
+ */
+var Injectigator = this.Injectigator = {};
 
-II.isNode = function(node) {
+
+/**
+ * Whether the passed in function is an InjectigatorNode.
+ * @param {Function} node A function that may be a {@see #InjectigatorNode}.
+ * @returns {boolean} Whether the function passed is an InjectigatorNode.
+ */
+Injectigator.isNode = function(node) {
   return !!node.$jsrNode;
 };
 
-II.enter = function(node) {
-  node.$called++;
+
+/**
+ * Records the start time and links the prev/next and first/last node and 
+ * child of the node and the parent node, respectively.
+ * @param {Function} node An InjectigatorNode (function).
+ * @returns {Object} The Injectigator class.
+ */
+Injectigator.enter = function(node) {
   node.$start = new Date;
-  if (!II.head.length) {
-    II.head.push(node);
+  node.$end = null;
+  
+  node.$called++;
+  
+  // If the previous node/function hadn't finished executing, then this new
+  // node is a sub-node/fn of the previous. NOTE(ibolmo): for the async. case
+  // the nodes are treated differently.
+  var parent = (!prevNode.$end) ? prevNode : prevNode.parent;  
+  if (!parent.$first) {
+    parent.$first = node; 
   }
+  parent.$last = node;
+  
+  node.$prev = prevNode;
+  prevNode = prevNode.$next = node;
   return this;
 };
 
-II.exit = function(node, result) {
+
+/**
+ * Records the end, elapsed time, and resets the parent if the sub-nodes
+ * finished executing.
+ * @param {Function} node An InjectigatorNode.
+ * @param {*} result The result of calling the function.
+ * @returns {*} The result of the original function.
+ */
+Injectigator.exit = function(node, result) {
   node.$end = new Date;
   node.$elapsed[node.$called] = node.$end - node.$start;
+
+  if (prevNode.parent == node) {
+    prevNode = node;
+  }
   return result;
 };
 
-II.fn = function(fn) {
+
+/**
+ * Wraps a normal function with the {@see #enter} and {@see #exit}
+ * Injectigator utility functions.
+ * @param {Function} fn The original function.
+ * @returns {Function} An InjectigatorNode.
+ */
+
+Injectigator.fn = function(fn) {
   var node = function() {
-    return II.enter(node).exit(node, fn.apply(this, arguments));
+    return Injectigator.enter(node).exit(node, fn.apply(this, arguments));
   };
   node.$jsrNode = true;
   node.$called = 0;
@@ -31,10 +84,15 @@ II.fn = function(fn) {
   return node;
 };
 
+
+// Initialize prevNode the "root" node.
+var prevNode = Injectigator.ROOT = Injectigator.fn();
+prevNode.parent = prevNode;
+
 // Override setInterval or setTimeout. Helpful for the visualization stage.
 var oI = this.setInterval;
 this.setInterval = function(fn, interval){
-  if (II.isNode(fn)) {
+  if (Injectigator.isNode(fn)) {
     fn.$periodical = true; 
   }
   return oI(fn, interval);
@@ -42,7 +100,7 @@ this.setInterval = function(fn, interval){
 
 var oT = this.setTimeout;
 this.setTimeout = function(fn, timeout) {
-  if (II.isNode(fn)) {
+  if (Injectigator.isNode(fn)) {
     fn.$delayed = true;
   }
   return oT(fn, timeout);
