179cb41091dd69e543b415e19395b2c988fb654b
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-10-31 00:00:44 -0700

    added jsTestDriver.conf and _test file

---
+++ injectigator_test.js
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview The Injectigator Unit Tests. See 
+ * {@ link http://code.google.com/p/js-test-driver/wiki/GettingStarted} on how
+ * to get started.
+ *
+ * @author ibolmo@gmail.com (Olmo Maldonado)
+ */
+ 
