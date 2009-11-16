91d7f068361d24215505726092d332d2d5a8fe31
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-11-16 10:46:19 -0800

    primitive asciiview tests pass

--- asciiview.js
+++ asciiview.js
@@ -19,7 +19,6 @@
  *                                                 ├ doSomething, 100 ms
  *                      ╠ log (periodical), 10 ms
  * ├ anon (210), 10 ms
- * ┘
  *
  * @param {Object} opt_root An optional root to use instead the global
  *     Injectigator root.
@@ -56,6 +55,13 @@ AsciiView.SymbolType = {
 AsciiView.prototype.l_ = 0;
 
 
+/**
+ * Recursively parses the node and its children and in the process populates the
+ * the string buffer with the output. The {@see #toString} method returns the
+ * result.
+ *
+ * @param {Object} node The node to parse.
+ */
 AsciiView.prototype.parseNode = function(node) {
   var prefix = this.pad_(this.buffer_[this.l_].length);
   while (node) {
@@ -67,8 +73,7 @@ AsciiView.prototype.parseNode = function(node) {
       ].join(' ');
     }
     if (node.first) {
-      this.buffer_[this.l_] += prefix +
-                               (node.curry ? ' ' : '') +
+      this.buffer_[this.l_] += (node.curry ? ' ' : '') +
                                AsciiView.SymbolType.START;
       this.parseNode(node.first);
     }
@@ -79,12 +84,15 @@ AsciiView.prototype.parseNode = function(node) {
 
 /** @return The ASCII representation of the graph. */
 AsciiView.prototype.toString = function() {
-  var str = this.buffer_.join('\n');
-  console.log(str);
-  return str;
+  return this.buffer_.join('\n');
 };
 
 
+/**
+ * Returns a string composed of spaces.
+ * @param {number} len The length of the string.
+ * @return {string} The pad string.
+ */
 AsciiView.prototype.pad_ = function(len) {
   return new Array(len).join(' ');
 };
--- asciiview_test.js
+++ asciiview_test.js
@@ -1,3 +1,10 @@
+/**
+ * @fileoverview AsciiView Unit Tests. See the Injectigator Unit Tests for
+ * information on how to run the tests.
+ *
+ * @author ibolmo@gmail.com (Olmo Maldonado)
+ */
+
 
 (function() {
 
@@ -49,7 +56,23 @@ TestCase('AsciiView', {
   },
 
   testParseNodeNestedChildren: function() {
-
+    var root = {};
+    root.first = node('parent', 0, node('sibling', 0));
+    var second = node('2nd', 0, node('3rd', 0));
+    root.first.first = node('1st', 0, second);
+    second.first = node('a', 0, node('b', 0));
+    var expected = [
+      this.symbols.START,
+      this.symbols.NODE + ' parent, 0 ms ' + this.symbols.START,
+      '               ' + this.symbols.NODE + ' 1st, 0 ms',
+      '               ' + this.symbols.NODE + ' 2nd, 0 ms ' + this.symbols.START,
+      '                           ' + this.symbols.NODE + ' a, 0 ms',
+      '                           ' + this.symbols.NODE + ' b, 0 ms',
+      '               ' + this.symbols.NODE + ' 3rd, 0 ms',
+      this.symbols.NODE + ' sibling, 0 ms'
+    ].join('\n');
+    this.view.parseNode(root);
+    assertEquals(expected, this.view.toString());
   }
 
 });
