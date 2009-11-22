62074bdd1b2967be5346cded14b29d152af3a7ca
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-11-21 17:49:29 -0800

    Taking shape

--- injectigator.py
+++ injectigator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright (C) 2009- Olmo Maldonado
 # Author: Olmo Maldonado
 # Contact: ibolmo@gmail.com
@@ -5,25 +7,81 @@
 """Injectigator JS (pre) Processing Module
 
 This can be run as library or as a standalone application.
+
+Standalone Usage:
+./injectigator.py -h
+
 """
+
+__version__ = '0.0.1'
 
 import logging
 import optparse
+import os
+
+def _print_header():
+  """Return the header text for a processed JS"""
+  return '''
+  /**
+   * This file has been modified by Injectigator (v. %(version)s).
+   * For more information: http://github.com/ibolmo/Injectigator
+   */
+  ''' % {'version': __version__}
+
+def process(js_file):
+  """Process the JS file and inject the Injectigator API in the JS code.
+
+  Arguments:
+  js_file -- the js file handler
+
+  Returns:
+  processed_js -- the processed JavaScript in a string.
+
+  """
+  processed_js = _print_header()
+
+
+def _validate_input(parser, args):
+  """Checks that the length is correct and each input is a file."""
+  if len(args) != 2:
+    parser.print_help()
+    parser.exit()
+
+  input_file = os.path.abspath(args[0])
+  output_file = os.path.abspath(args[1])
+
+  if not os.path.isfile(input_file):
+    parser.exit('Invalid input file: %s' % input_file)
+
+  if not os.path.exists(os.path.dirname(output_file)):
+    parser.exit('Ouput file directory, %s, is invalid' % output_dir )
+
+  return input_file, output_file
+
 
 def main():
   """The entrypoint when using this file as an entry point."""
   logging.basicConfig(format='injectigator.py: %(message)s', level=logging.INFO)
-  
-  usage = 'usage: %prog [options] output_file.js'
+
+  usage = 'Usage: %prog [options] input_file output_file'
   parser = optparse.OptionParser(usage)
-  parser.add_option('-i',
-                    '--input',
-                    dest='inputs',
-                    action='append',
-                    help='The input file to process.')
+  # TODO(ibolmo): Advanced options.
   (options, args) = parser.parse_args()
-  
-  
+
+  input_file, output_file = _validate_input(parser, args)
+
+  input_file = open(input_file)
+  processed_js = None
+  try:
+    processed_js = process(input_file)
+  else:
+    input_file.close()
+    parser.error('An unexpected error occurred while reading the input file.')
+
+  if processed_js is not None:
+    output_file = open(output_file, 'w')
+    output_file.write(processed_js)
+    output_file.close()
 
 if __name__ == '__main__':
   main()
---
+++ injectigator_test.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2009- Olmo Maldonado
+# Author: Olmo Maldonado
+# Contact: ibolmo@gmail.com
+
+"""Injectigator JS (pre) Processing Module Unit Tests"""
+
+def testInject():
+  pass
+  
